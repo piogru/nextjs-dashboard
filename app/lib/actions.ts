e399e36cb0b73898f173f50115cb169b48922ce1
Piotr G <81086063+piogru@users.noreply.github.com>
2024-11-30 02:59:31 +0100

    feat: implement authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -94,7 +96,7 @@ async function updateInvoice(id: string, prevState: State, formData: FormData) {
   redirect("/dashboard/invoices");
 }
 
-export async function deleteInvoice(id: string) {
+async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
@@ -106,4 +108,19 @@ export async function deleteInvoice(id: string) {
   }
 }
 
-export { createInvoice, updateInvoice };
+async function authenticate(prevState: string | undefined, formData: FormData) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+  }
+}
+
+export { createInvoice, updateInvoice, deleteInvoice, authenticate };
